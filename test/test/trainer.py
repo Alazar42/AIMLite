38679695b9fd9f98a920e848b5ced05479977e0a
atocodes <atocodes@gmail.com>
2026-09-19 14:49:20 +0300

    feat(serve): add dedicated interactive chat playground at /chat and remove scaffolded test folder

--- test/test/trainer.py
+++
@@ -1,8 +0,0 @@
-"""Document & Knowledge QA (RAG Paradigm): trainer.py"""
-
-from aimlite.rag import RAGTrainer
-
-
-class IndexBuilderTrainer(RAGTrainer):
-    """Trainer orchestrator building and persisting the semantic vector index."""
-    pass
